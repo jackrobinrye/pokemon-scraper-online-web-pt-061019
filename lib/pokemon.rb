55a8bd6080e4d400c7b47c35692e480bd8724f16
Jack Atherton <jackrrye@gmail.com>
2019-08-22 19:28:13 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,9 +2,9 @@ class Pokemon
 
   def initialize(id:, name:, type:, db:)
     :id = id
-    @name = name
-    @type = type
-    @db = db
+    :name = name
+    :type = type
+    :db = db
   end
 
 end
