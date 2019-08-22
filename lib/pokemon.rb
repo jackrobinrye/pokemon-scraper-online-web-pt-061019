1a9e3ae71c07c00e2ee7e0ff95d4d44e7cade6ed
Jack Atherton <jackrrye@gmail.com>
2019-08-22 20:41:34 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -9,4 +9,14 @@ class Pokemon
     @db = db
   end
 
+  def self.save 
+    if self.id 
+    else 
+      sql = <<-SQL 
+      INSERT INTO pokemon
+      (name, type)
+      VALUES (?, ?)
+      SQL 
+      
+
 end
